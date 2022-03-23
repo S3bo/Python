d44bb832ba7afa3ee63d340d94ef2088b066ec53
Kas234 <s.janczura@op.pl>
2022-03-23 16:22:19 +0100

    sortowanie

---
+++ sortowanie.py
@@ -0,0 +1,20 @@
+#Lista imion + sortowanie alfabetyczne + sprawdzenie liczby elementow
+
+list = ("marta", "mikolaj","adam", "alicja", "andrzej", "dominik","hania",
+        "katarzyna","krzysztof","lukasz","malgorzata","rafal","sebastian",
+        "anka","dominik","martyna","tymoteusz")
+
+result =sorted(list)
+print(len(list))
+print(type(list))
+print(result)
+
+
+
+'''
+sample_set = {"Żółty", "Pomarańczowy", "Czarny"}
+sample_list = ["Niebieski", "Zielony", "Czerwony"]
+
+res_dict = dict(zip(sample_set,sample_list))
+print(res_dict)
+'''
